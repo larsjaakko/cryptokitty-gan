26c7fc3f41db2e7f98138a3344ab5692ccbfd01b
Lars Jaakko Havro <larsjh@gmail.com>
2018-09-07 21:52:29 +0200

    Improved data fetcher to use a pool of 4 workers.

--- python/dataset/fetch_dataset.py
+++ python/dataset/fetch_dataset.py
@@ -4,7 +4,7 @@ import requests
 import json
 import time
 
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Pool
 
 from pathlib import Path, PurePath, PosixPath
 import os
@@ -16,102 +16,97 @@ out_folder = Path.cwd() / 'data/processed_png'
 
 dataset_size = 500000
 
-def fetcher(q):
+def get_download_list():
+
+    print("Generating download list...")
+
+    existing = sorted(out_folder.glob('*.png'))
+    existing_ids = []
+
+    for i, file in enumerate(existing):
+
+        file = PurePath(file)
+
+        existing_ids.append(file.stem)
+
+    existing_ids = np.array(existing_ids, dtype=int)
+    downloaded = len(existing_ids)
+
+    id_pool = np.arange(1, 900000)
+    id_pool = np.setdiff1d(id_pool, existing_ids)
+
+    dl_list = np.random.choice(id_pool,
+                                size=dataset_size-downloaded,
+                                replace=False)
+
+    return dl_list
+
+
+def worker(file_id):
     """
-    Fetches images to be consumed and waits for the consumer
-    to finish processing
+    Each process fetches an image before converting it. It should be
+    faster (and way simpler) to run the end-to-end process in parallell than
+    using a queue object between a downloader and a converter functions
     """
-    downloaded = 0
-    id_list = np.arange(900000)
 
-    while downloaded <= dataset_size:
+    print('Fetching kitty #{}'.format(file_id))
 
-        next_id = np.random.choice(id_list, replace=False)
+    try:
+        url = rf.get_url(file_id)
+        file = rf.fetch_file(url, file_id)
 
-        print('Download #{}:\tFetching kitty #{}'.format(downloaded+1, next_id))
+    except (requests.exceptions.RequestException, json.decoder.JSONDecodeError) as e:
+        print(e)
+    except Exception as e:
+        print(e.__doc__)
+        print(e.message)
 
-        try:
-            url = rf.get_url(next_id)
-            file = rf.fetch_file(url, next_id)
-            downloaded += 1
-            q.put(file)
-
-        except (requests.exceptions.RequestException, json.decoder.JSONDecodeError) as e:
-            print(e)
-            continue
-        except Exception as e:
-            print(e.__doc__)
-            print(e.message)
-            continue
+    convert(file)
 
 
-    if downloaded > dataset_size:
-        q.put(-1)
-
-
-def converter(q):
+def convert(filestr):
 
     """
-    Consumes file list and converts them using the inkscape
-    command line interface
+    Converts the given path to png.
     """
-    while True:
 
-        filestr = q.get()
+    try:
+        file = PurePath(filestr)
+        path = Path(file)
 
-        if filestr == -1:
-            print("OK, we're done here!")
-            break
+    except Exception as e:
+        print(e)
+        print("Sleeping for a bit before retrying the conversion process..")
+        time.sleep(10)
 
-        try:
-            file = PurePath(filestr)
-            path = Path(file)
+    if file.suffix == '.png':
+        print('Kitty #{} was a .png! Deleting this troll kitten.'.format(file.stem))
+        path.unlink()
 
-        except TypeError as e:
-            print(e)
-            print("Sleeping for a bit before retrying the convert process..")
+    input = filestr
+    output = str(out_folder / file.stem) + '.png'
 
-            time.sleep(10)
-            continue
+    inkscape_string = 'inkscape -z {} -e {} -w 256 -b "#ffffff"'.format(input, output)
 
-        if file.suffix == '.png':
-            print('Kitty #{} was a .png! Deleting this troll kitten.'.format(file.stem))
-            path.unlink()
-            continue
-
-        input = filestr
-        output = str(out_folder / file.stem) + '.png'
-
-        inkscape_string = 'inkscape -z {} -e {} -w 256 -b "#ffffff"'.format(input, output)
+    if file.suffix == '.svg':
 
         try:
             subprocess.run(inkscape_string, shell=True, check=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-
             print('Converted kitty #{}!'.format(file.stem))
 
         except Exception as e:
+            print("Skipped conversion of kitty #{} for some error: {}".format(file.stem, e.__doc__))
 
-            print("Skipped conversion of kitty #{} for some error".format(file.stem))
-            print(e.__doc__)
-
-        #path.unlink()
+    path.unlink()
 
 
 def main():
 
-    q = Queue()
+    download_list = get_download_list()
 
-    fetch_process = Process(target=fetcher, args=(q,))
-    convert_process = Process(target=converter, args=(q,))
+    p = Pool(4)
+    p.map(worker, download_list)
 
-    fetch_process.start()
-    convert_process.start()
-
-    q.close()
-    q.join_thread()
-
-    fetch_process.join()
-    convert_process.join()
 
 if __name__ == "__main__":
     main()
